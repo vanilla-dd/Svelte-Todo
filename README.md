a42de2f4f8164d0f86893c2508eb0740c3764398
whiteHawk <freeidlll6@gmail.com>
2023-07-13 17:09:55 +0530

    🥳 lil bit of html and CSs

---
+++ README.md
@@ -0,0 +1,1 @@
+### First time trying svelte i tried react couple days before and its a mess so i hope svelte would be better.
---
+++ src/routes/todo/+page.svelte
@@ -0,0 +1,30 @@
+<!-- importing components -->
+<script>
+	import Header from '../../components/Header.svelte';
+	import Form from '../../components/Form.svelte';
+	import Todos from '../../components/Todos.svelte';
+</script>
+
+<!-- main container -->
+<div id="app-container" class="app-container">
+	<Header />
+	<Todos />
+	<Form />
+</div>
+
+<!-- styles for main container -->
+<style>
+	.app-container {
+		width: 400px;
+		min-height: 500px;
+		background-color: #282c34;
+		box-shadow: 0 20px 80px rgba(0, 0, 0, 0.6);
+		background: radial-gradient(circle, #282c34 0%, rgba(40, 48, 56, 1) 100%);
+		position: relative;
+		border-radius: 1em;
+		padding: 20px;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+</style>
